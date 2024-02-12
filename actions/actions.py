47968d5be09b6d7ec8b27b330712389b3e32caf5
Alejandro Chitiva Castillo <83592808+AlejandroChitivaC@users.noreply.github.com>
2024-02-12 10:37:31 -0500

    Chatbot made in python with RASA, trained

---
+++ actions/actions.py
@@ -0,0 +1,54 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionMostrarOpcion1(Action):
+    def name(self) -> Text:
+        return "opcion 1"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_greet_mostrar_opcion1")
+        return []
+
+
+class ActionMostrarOpcion2(Action):
+    def name(self) -> Text:
+        return "action_mostrar_opcion2"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_greet_mostrar_opcion2")
+        return []
---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from rasa.core.agent import Agent
+
+app = Flask(__name__)
+rasa_agent = Agent.load("models")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get_response", methods=["POST"])
+async def get_response():
+    user_message = request.form["user_message"]
+    responses = await rasa_agent.handle_text(user_message)
+    bot_response = responses[0]["text"]
+    return {"response": bot_response}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
