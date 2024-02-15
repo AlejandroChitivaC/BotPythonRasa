25e085fb5c08f1290617be6e7f45743e15754f1f
Alejandro Chitiva Castillo <83592808+AlejandroChitivaC@users.noreply.github.com>
2024-02-15 13:16:50 -0500

    Rasa training model, frontend click events according to the bot response.

--- app.py
+++ app.py
@@ -16,6 +16,17 @@ async def get_response():
     responses = await rasa_agent.handle_text(str(user_message))
     bot_response = responses[0]["text"]
     return {"response": bot_response}
+# async def get_response():
+#     user_message = request.form["user_message"]
+#     responses = await rasa_agent.handle_text(str(user_message))
+#
+#     # Obtén la respuesta del bot
+#     bot_response = responses[0]["text"]
+#
+#     # Obtén los botones, si están disponibles
+#     buttons = responses[0].get("buttons", [0])
+#
+#     return {"response": bot_response, "buttons": buttons}
 
 
 if __name__ == "__main__":
