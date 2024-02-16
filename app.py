903b03775bbafd9461178099ff759580546f07f2
Alejandro Chitiva Castillo <83592808+AlejandroChitivaC@users.noreply.github.com>
2024-02-16 14:30:15 -0500

    Rasa training model, frontend API  external connection

--- app.py
+++ app.py
@@ -1,32 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
+from flask_cors import CORS
 from rasa.core.agent import Agent
 
 app = Flask(__name__)
+CORS(app)
 rasa_agent = Agent.load("models")
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
+# @app.route("/api")
+# def home():
+#     return render_template("index.html")
 @app.route("/get_response", methods=["POST"])
 async def get_response():
     user_message = request.form["user_message"]
-    responses = await rasa_agent.handle_text(str(user_message))
+    responses = await rasa_agent.handle_text(user_message)
     bot_response = responses[0]["text"]
     return {"response": bot_response}
-# async def get_response():
-#     user_message = request.form["user_message"]
-#     responses = await rasa_agent.handle_text(str(user_message))
-#
-#     # Obtén la respuesta del bot
-#     bot_response = responses[0]["text"]
-#
-#     # Obtén los botones, si están disponibles
-#     buttons = responses[0].get("buttons", [0])
-#
-#     return {"response": bot_response, "buttons": buttons}
 
 
 if __name__ == "__main__":
