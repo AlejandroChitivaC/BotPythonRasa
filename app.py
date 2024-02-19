726564a3bef94bdd6562884f92d2db6df89e6068
Alejandro Chitiva Castillo <83592808+AlejandroChitivaC@users.noreply.github.com>
2024-02-19 08:30:24 -0500

    Rasa training model, frontend API  external connection

--- app.py
+++ app.py
@@ -7,15 +7,18 @@ CORS(app)
 rasa_agent = Agent.load("models")
 
 
-# @app.route("/api")
-# def home():
-#     return render_template("index.html")
+# @app.route("/get_response", methods=["POST"])
+# async def get_response():
+#     user_message = request.form["user_message"]
+#     responses = await rasa_agent.handle_text(user_message)
+#     bot_response = responses[0]["text"]
+#     return {"response": bot_response}
 @app.route("/get_response", methods=["POST"])
 async def get_response():
     user_message = request.form["user_message"]
     responses = await rasa_agent.handle_text(user_message)
-    bot_response = responses[0]["text"]
-    return {"response": bot_response}
+    bot_responses = [response["text"] for response in responses]
+    return {"responses": bot_responses}
 
 
 if __name__ == "__main__":
